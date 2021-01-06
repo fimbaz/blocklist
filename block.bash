4d9f76023fddf071c5823dbf11d45be3dee9a36f
root <root@debian>
2021-01-05 20:49:35 -0500

    loook

--- block.bash
+++ block.bash
@@ -3,4 +3,9 @@ rm blocklist;
 while read URL; do
 curl -s -L "$URL" | gunzip -c >> blocklist
 done < blocklisturls
-
+ipset destroy blacklist
+ipset create blacklist hash:net hashsize 131072
+while read NET; do
+    ipset -A blacklist $NET;
+done < blocklist
+iptables -A INPUT -m set --match-set blacklist src -j DROP
